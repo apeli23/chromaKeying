81435153da3860e96bfd4fce125484d96e5b6e5a
apeli23 <apelibrian@gmail.com>
2022-02-06 13:06:58 +0300

    video-export-function

--- components/Processor.jsx
+++ components/Processor.jsx
@@ -1,11 +1,14 @@
-import React, {useEffect } from 'react';
+import { useState, useRef } from 'react';
+import { Canvas2Video } from "canvas2video";
 
 export default function Processor() {
     let video, canvas, outputContext, temporaryCanvas, temporaryContext;
+    const canvasRef = useRef();
+    const [computed, setComputed] = useState(false);
 
     function computeFrame() {
-        video =  document.getElementById("video")
-        
+        video = document.getElementById("video")
+
         temporaryCanvas = document.createElement("canvas");
         temporaryCanvas.setAttribute("width", 800);
         temporaryCanvas.setAttribute("height", 450);
@@ -13,7 +16,7 @@ export default function Processor() {
 
         canvas = document.getElementById("output-canvas");
         outputContext = canvas.getContext("2d");
-        
+
         temporaryContext.drawImage(video, 0, 0, video.width, video.height);
         let frame = temporaryContext.getImageData(0, 0, video.width, video.height);
 
@@ -27,16 +30,42 @@ export default function Processor() {
             }
         }
         outputContext.putImageData(frame, 0, 0)
-        setTimeout(computeFrame, 0)
+        setTimeout(computeFrame, 0);
+        setComputed(true);
     }
+
+    function uploadHandler() {
+        const chunks = []; // here we will store our recorded media chunks (Blobs)
+        const cnv = canvasRef.current;
+        const stream = cnv.captureStream(); // grab our canvas MediaStream
+        const rec = new MediaRecorder(stream); // init the recorder
+        // every time the recorder has new data, we will store it in our array
+        rec.ondataavailable = e => chunks.push(e.data);
+        // only when the recorder stops, we construct a complete Blob from all the chunks
+        rec.onstop = e => exportVid(new Blob(chunks, { type: 'video/webm' }));
+        rec.start();
+        setTimeout(() => rec.stop(), 3000); // stop recording in 3s
+    }
+    function exportVid(blob) {
+        const vid = document.createElement('video');
+        vid.src = URL.createObjectURL(blob);
+        vid.controls = true;
+        document.body.appendChild(vid);
+        const a = document.createElement('a');
+        a.download = 'myvid.webm';
+        a.href = vid.src;
+        a.textContent = 'download the video';
+        document.body.appendChild(a);
+    }
+
     return (
         <>
-            <header class="header">
-                <div class="text-box">
-                    <h1 class="heading-primary">
-                        <span class="heading-primary-main">Cloudinary Chroma Keying</span>
+            <header className="header">
+                <div className="text-box">
+                    <h1 className="heading-primary">
+                        <span className="heading-primary-main">Cloudinary Chroma Keying</span>
                     </h1>
-                    <a href="#" class="btn btn-white btn-animated" onClick={computeFrame}>Remove Background</a>
+                    <a href="#" className="btn btn-white btn-animated" onClick={computeFrame}>Remove Background</a>
                 </div>
             </header>
             <div className="row">
@@ -44,7 +73,8 @@ export default function Processor() {
                     <video className="video" crossOrigin="Anonymous" src='https://res.cloudinary.com/dogjmmett/video/upload/v1632221403/sample_mngu99.mp4' id='video' width='400' height='360' controls autoPlay muted loop type="video/mp4" />
                 </div>
                 <div className="column">
-                    <canvas className="canvas"  id="output-canvas" width="500" height="360" ></canvas>
+                    <canvas className="canvas" ref={canvasRef} id="output-canvas" width="500" height="360" ></canvas><br />
+                    {computed && <a href="#" className="btn btn-white btn-animated" onClick={uploadHandler}>Save</a>}
                 </div>
             </div>
         </>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Processor from '../components/Processor'
-import Template from '../components/Template'
 export default function Home() {
   return (
     <div>
