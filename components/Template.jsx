8b311611c01eefb6be9d34fb1aadc4943decc364
apeli23 <apelibrian@gmail.com>
2022-02-06 11:40:49 +0300

    css-styling

--- components/Template.jsx
+++ components/Template.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect } from 'react';
 
-export default function Processor() {
+export default function Template() {
     let video, canvas, outputContext, temporaryCanvas, temporaryContext;
 
     function computeFrame() {
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Processor from '../components/Processor'
+import Template from '../components/Template'
 export default function Home() {
   return (
     <div>
